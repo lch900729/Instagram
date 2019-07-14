7f6cb283ac20e847fe2d6f3b55651d614da6afe0
Chen Liang <chenl@fb.com>
2019-07-14 10:40:05 -0700

    CRUD finish

--- Insta/models.py
+++ Insta/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
@@ -9,4 +10,7 @@ class Post(models.Model):
         format = 'JPEG',
         blank = True,
         null = True,)
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.title)])
     
